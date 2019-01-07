2b89bfa66c7d4449db7c53effb8ee54d285536da
YauheniRusinchuk <ruevgal@gmail.com>
2019-01-08 00:03:18 +0300

    veiws search

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.db.models import Q
+from apps.article.models import Article
 
 # Create your views here.
 class Search(View):
@@ -7,4 +9,14 @@ class Search(View):
     ''' Search view '''
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'search/search.html', {})
+        query = request.GET.get('q')
+        if query:
+            articles = Article.objects.filter(
+                Q(text__icontains=query) |
+                Q(text__startswith=query) |
+                Q(text__istartswith=query) |
+                Q(text__endswith=query) |
+                Q(text__iendswith=query)
+            )
+            return render(request, 'search/search.html', {'articles': articles})
+        return redirect('home:home_page')
