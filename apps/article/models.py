4991b0de133a4a0b249bf3e5f4884bde10a013f6
YauheniRusinchuk <ruevgal@gmail.com>
2019-01-08 16:11:39 +0300

    delete comments view

--- apps/article/models.py
+++ apps/article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 from django.contrib.auth.models import User
 import apps.comments.models
 
@@ -10,6 +11,11 @@ class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(blank=False)
     date = models.DateTimeField(auto_now_add=True)
+
+
+    def get_absolute_url(self):
+        return reverse('home:detail_article', kwargs={'pk': self.pk})
+
 
     class Meta:
         ordering = ['-date']
--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.views import View
+from django.http import HttpResponseRedirect
 from apps.comments.models import Comment
 
 # Create your views here.
 class DeleteComments(View):
 
     ''' Delete comment '''
-
+    
 
     def get(self, request, *args, **kwargs):
         comment = Comment.objects.get(pk=kwargs['pk'])
         if request.user == comment.author:
             comment.delete()
-        return redirect('home:home_page')
+        return HttpResponseRedirect(reverse('home:detail_article', kwargs={'pk': comment.article.pk}))
