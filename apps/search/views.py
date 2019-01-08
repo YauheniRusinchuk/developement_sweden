e8349aa2768d2b03892f5a1d8042197dc784b7ba
YauheniRusinchuk <ruevgal@gmail.com>
2019-01-09 00:06:03 +0300

    add

--- apps/search/views.py
+++ apps/search/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.db.models import Q
 from apps.article.models import Article
+from django.contrib.auth.models import User
 
 # Create your views here.
 class Search(View):
@@ -18,5 +19,19 @@ class Search(View):
                 Q(text__endswith=query) |
                 Q(text__iendswith=query)
             )
-            return render(request, 'search/search.html', {'articles': articles})
+            users = User.objects.filter(
+                Q(first_name__icontains=query) |
+                Q(first_name__startswith=query) |
+                Q(first_name__istartswith=query) |
+                Q(first_name__endswith=query) |
+                Q(first_name__iendswith=query) |
+                Q(last_name__icontains=query) |
+                Q(last_name__startswith=query) |
+                Q(last_name__istartswith=query) |
+                Q(last_name__endswith=query) |
+                Q(last_name__iendswith=query)
+
+            )
+            print(users)
+            return render(request, 'search/search.html', {'articles': articles, 'users': users})
         return redirect('home:home_page')
