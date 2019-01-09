aefe79fee04a84f954879cb88f96bb0ec1a0933c
YauheniRusinchuk <ruevgal@gmail.com>
2019-01-09 15:11:19 +0300

    new version

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -3,11 +3,17 @@ from django.views import View
 from django.http import HttpResponseRedirect
 from apps.comments.models import Comment
 
+
+class UpdateComment(View):
+    pass
+
+
+
 # Create your views here.
 class DeleteComments(View):
 
     ''' Delete comment '''
-    
+
 
     def get(self, request, *args, **kwargs):
         comment = Comment.objects.get(pk=kwargs['pk'])
--- apps/static/js/comments.js
+++ apps/static/js/comments.js
@@ -1,14 +1,7 @@
 $(function(){
 
-    $('#btn_edit_comment').click((e)=> {
-        e.preventDefault();
-        $('.open_form_edit').show('fast');
-    })
+    
 
-    $('#close_form').click((e)=>{
-        e.preventDefault();
-        $('.form_new_comment')[0].reset();
-        $('.open_form_edit').hide('fast');
-    })
+
 
 });
