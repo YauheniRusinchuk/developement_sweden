e72cba3e4371b97f865670b0c5e555433f1896ff
YauheniRusinchuk <ruevgal@gmail.com>
2019-01-31 03:38:31 +0300

    test url

--- apps/tests/test_urls.py
+++ apps/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from apps.home.views import Home
+from apps.home.views import Home, NewPost
 
 
 class TestUrls(SimpleTestCase):
@@ -8,3 +8,8 @@ class TestUrls(SimpleTestCase):
     def test_url_simple(self):
         url = reverse('home:home_page')
         self.assertEquals(resolve(url).func.view_class, Home)
+
+
+    def test_url_add_post(self):
+        url = reverse('home:newpost_page')
+        self.assertEquals(resolve(url).func.view_class, NewPost)
